52d785fec384a3c27bf2c0885a59bcc855152883
james <james@devpipeline.com>
2023-03-10 14:10:46 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+/* 
+Due: Monday the 13th
+Project: Slot Machine
+- Rules can be whatever you want
+- Symbols can be whatever you want (&, *, (, #, etc..)
+- Must include some promise object
+- Must include an async function that awaits a promise
+- Starting balance, betting system, continue playing by choice, scoring system
+- IDEA: Animate the console when you "pull the lever"
+  - HINT: combination of console.log and console.clear()
+
+  * & &
+[ $ $ $ ] <-
+  * ( *
+*/
